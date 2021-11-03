9c5c7a50380499b2310a33a780dd3d7ceaeaa288
JRKap <JRKap@10.0.0.2>
2021-11-03 15:43:14 -0600

    Lab 7

--- src/java/servlets/UserServlet.java
+++ src/java/servlets/UserServlet.java
@@ -6,6 +6,7 @@
 package servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
@@ -13,6 +14,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import models.User;
 import services.RoleService;
 import services.UserService;
 
@@ -27,8 +29,25 @@ public class UserServlet extends HttpServlet
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException
-    {   
+    {
+        
+        HttpSession session = request.getSession();
+        
+        try {
+            ArrayList<User> userList = new UserService().getAll();
+        } catch (Exception ex) {
+            Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            ArrayList<User> userList = new UserService().getAll();
+                session.setAttribute("usersList", userList);
+                request.setAttribute("usersList", userList);
+            } catch (Exception ex) {
+                Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
+            }
         getServletContext().getRequestDispatcher("/WEB-INF/userManager.jsp").forward(request, response);
+        return;
+        
     }
 
     @Override
@@ -43,29 +62,77 @@ public class UserServlet extends HttpServlet
         String password = request.getParameter("addPassword");
         String userType = request.getParameter("addUserType");
         Boolean active = false;
-        
-        if(request.getParameter("active") == "active"){
+
+        if(request.getParameter("action").equals("edit")){
+            request.setAttribute("message", "Edit peen pressed bitch");
+            
+            String ogEmail = request.getParameter("selected");
+            UserService editUser = new UserService(); 
+            User tempUser = new User();
+            
+            try {
+                tempUser = editUser.get(ogEmail);
+                
+                request.setAttribute("editEmail", tempUser.getEmail());
+                request.setAttribute("editFName", tempUser.getFirstName());
+                request.setAttribute("editLName", tempUser.getLastName());
+                request.setAttribute("editPassword", tempUser.getPassword());
+                request.setAttribute("ogEmail", ogEmail);
+            } catch (Exception ex) {
+                Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            getServletContext().getRequestDispatcher("/WEB-INF/userManager.jsp").forward(request, response);
+            return;
+        }else if (request.getParameter("action").equals("delete")){
+            request.setAttribute("message", "delete peen pressed bitch");
+            try {
+            String ogEmail = request.getParameter("selected");
+            UserService editUser = new UserService(); 
+            User tempUser = new User(); 
+            tempUser = editUser.get(ogEmail);
+            
+            if(request.getParameter("selected") == null){
+                request.setAttribute("message", "please enter a valid email");
+            
+                getServletContext().getRequestDispatcher("/WEB-INF/userManager.jsp").forward(request, response);
+             return;
+            }else{
+                
+                new UserService().delete(ogEmail);
+                getServletContext().getRequestDispatcher("/WEB-INF/userManager.jsp").forward(request, response);
+             return;
+            }
+            
+            } catch (Exception ex) {
+                Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            getServletContext().getRequestDispatcher("/WEB-INF/userManager.jsp").forward(request, response);
+            return;
+        }else if (request.getParameter("action").equals("add")){
+            if(request.getParameter("active") == "active"){
             active = true;
         }
         
         if(email == null || email.equals("")){
-            request.setAttribute("angy_message", "please enter a valid email");
+            request.setAttribute("message", "please enter a valid email");
             
             getServletContext().getRequestDispatcher("/WEB-INF/userManager.jsp").forward(request, response);
             return;
         }else if (firstName == null || firstName.equals("")){
-            request.setAttribute("angy_message", "please enter a valid email");
+            request.setAttribute("message", "please enter a valid email");
             
             getServletContext().getRequestDispatcher("/WEB-INF/userManager.jsp").forward(request, response);
             return;
         }else if (lastName == null || lastName.equals("")){
-            request.setAttribute("angy_message", "please enter a valid email");
+            request.setAttribute("message", "please enter a valid email");
             
             getServletContext().getRequestDispatcher("/WEB-INF/userManager.jsp").forward(request, response);
             return;
             
         }else if (password == null || password.equals("")){
-            request.setAttribute("angy_message", "please enter a valid email");
+            request.setAttribute("message", "please enter a valid email");
             
             getServletContext().getRequestDispatcher("/WEB-INF/userManager.jsp").forward(request, response);
             return;                
@@ -73,17 +140,72 @@ public class UserServlet extends HttpServlet
             
             try {
                 new UserService().insert(email, active, firstName, lastName, password, new RoleService().roleIDLookup(userType));
+                try {
+                    
+            ArrayList<User> userList = new UserService().getAll();
+                session.setAttribute("usersList", userList);
+                request.setAttribute("usersList", userList);
             } catch (Exception ex) {
                 Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
             }
+                
+                getServletContext().getRequestDispatcher("/WEB-INF/userManager.jsp").forward(request, response);
+            return;
+            } catch (Exception ex) {
+                Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+  
+        }
+        else if (request.getParameter("action").equals("updateUser")){
+            try{
+                
+            String ogEmail = request.getParameter("selected");
+            UserService editUser = new UserService(); 
+            User tempUser = new User(); 
+            tempUser = editUser.get(ogEmail);            
+                
+        if(request.getParameter("editEmail") == null){
+            request.setAttribute("message", "please enter a valid email");
+            
+            getServletContext().getRequestDispatcher("/WEB-INF/userManager.jsp").forward(request, response);
+            return;
+        }if (request.getParameter("editFName")== null){
+            request.setAttribute("message", "please enter a valid email");
+            
+            getServletContext().getRequestDispatcher("/WEB-INF/userManager.jsp").forward(request, response);
+            return;
+        }if (request.getParameter("editLName")== null){
+            request.setAttribute("message", "please enter a valid email");
+            
+            getServletContext().getRequestDispatcher("/WEB-INF/userManager.jsp").forward(request, response);
+            return;
+            
+        }if (request.getParameter("editPassword")== null){
+            request.setAttribute("message", "please enter a valid email");
+
+            getServletContext().getRequestDispatcher("/WEB-INF/userManager.jsp").forward(request, response);
+            return;                
+        }else{
             
             try {
-                session.setAttribute("usersList", new UserService().getAll());
-                request.setAttribute("usersList", new UserService().getAll());
+                
+                boolean editActive = false;
+                if(request.getParameter("active") == "active"){
+                    editActive = true;}
+                new UserService().update(ogEmail, request.getParameter("editEmail"),editActive,request.getParameter("editFName") , request.getParameter("editLName"), new RoleService().roleIDLookup(request.getParameter("editUserType")));
+                
+                ArrayList<User> userList = new UserService().getAll();
+                session.setAttribute("usersList", userList);
+                request.setAttribute("usersList", userList);
+  
+                getServletContext().getRequestDispatcher("/WEB-INF/userManager.jsp").forward(request, response);
+                    return;  
             } catch (Exception ex) {
                 Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
             }
-           
         }
+            }catch (Exception ex) {
+                Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
     }
-}
+}}}
--- web/WEB-INF/userManager.jsp
+++ web/WEB-INF/userManager.jsp
@@ -10,60 +10,76 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Huge Penis User Login, Edit, ADD page Fuck yea</title>
+        <link rel="stylesheet" href="${pageContext.request.contextPath}/css/styles.css" />
+        <title>Windows ME User Manager</title>
     </head>
     <body>
         <h1>Welcome</h1>
+        <h2>${message}</h2>
+        <div class="container" >
         <label for="addUserForm">Add Users</label>
             <form action="" method="post" id="addUserForm">
-                    <input type="text" name="addEmail" value="Email" id="addEmail"><br>
-                    <input type="text" name="addFName" value="First Name" id="addFName"><br>
-                    <input type="text" name="addLName" value="Last Name" id="addLName"><br>
+                    <input type="text" name="addEmail" placeholder="Email" id="addEmail"><br>
+                    <input type="text" name="addFName" placeholder="First Name" id="addFName"><br>
+                    <input type="text" name="addLName" placeholder="Last Name" id="addLName"><br>
                     <select name="active" id="active">
                         <option value="active" name="active">Active</option>
                         <option value="inactive" name="inactive">Inactive</option>
                     </select><br>
-                    <input type="text" name="addPassword" value="Password" id="addPassword"><br>
+                    <input type="text" name="addPassword" placeholder="Password" id="addPassword"><br>
                     <select name="addUserType" id="addUserType">
                         <option value="System Administrator">System Administrator</option>
                         <option value="Regular User">Regular User</option>
                         <option value="Company Administrator">Company Administrator</option>
-                        <option value="Alex Kapronczai">Alex Kapronczai</option>
-                        <option value="Julian Hill">Julian Hill</option>
                     </select>
-                    <input type="submit" value="addUser"> 
+                    <input type="submit" value="addUser">
+                    <input type="hidden" name="action" value="add">
             </form>
+        </div>
+            <div class="container">
             <label for="manageUsers"> Manage Users</label>
-            <c:if test="${usersList.size() != null}" >
-            <form action="" method="post" id="manageUsers">
-
-                <c:forEach items="${usersList}" var="tempUsers">
+            
+            
+            <table>
+                <c:forEach items="${usersList}" var="user">
                     <c:out value = "
-                        <li>
-                        <input type='radio' name='userListDisplayed' value='${tempUsers}'>
-                        <label for='userListDisplayed'>${tempUsers}</label>
-                        </li>
-                        " escapeXml="false">
-                    </c:out>
+                    
+                        <tr><td>${user.getEmail()}     ${user.getFirstName()}     ${user.getLastName()}    </td></tr>
+                        <tr><td><form action='' method='post'>
+                            <input type='submit' value='edit' name='edit'>
+                            <input type='hidden' name='selected' value='${user.getEmail()}'>
+                            <input type='hidden' name='action' id='action' value='edit'>
+                            </form></td></tr>
+                        <tr><td><form action='' method='post'>
+                            <input type='submit' value='delete' name='delete'>
+                            <input type='hidden' name='selected' value='${user.getEmail()}'>
+                            <input type='hidden' name='action' id='action' value='delete'>
+                            </form></td></tr>
+                    " escapeXml = "false" ></c:out>
                 </c:forEach>
-
-            </form>
+            </table>
+            </div>
+            <div class="container">
             <label for="editUserForm">Edit Users</label>
             <form action="" method="post" id="editUserForm">
-                <input type="text" name="" value="${usersList}" id="editEmail"><br>
-                <input type="text" name="" value="${usersList}" id="editFName"><br>
-                <input type="text" name="" value="${usersList}" id="editLName"><br>
-                <input type="text" name="" value="${usersList}" id="editPassword"><br>
-                <label for="editUserType">${usersList}</label>
-                <select name="User Type" id="editUserType">
-                    <option value="Sys Admin"></option>
-                    <option value="User"></option>
-                    <option value="CEO"></option>
-                    <option value="Alex Kapronczai"></option>
-                    <option value="Julian Hill"></option>
+                <input type="text" name="editEmail" value="${editEmail}" id="editEmail"><br>
+                <input type="hidden" name="selected" value="${ogEmail}">
+                <input type="text" name="editFName" value="${editFName}" id="editFName"><br>
+                <input type="text" name="editLName" value="${editLName}" id="editLName"><br>
+                <input type="text" name="editPassword" value="${editPassword}" id="editPassword"><br>
+                <label for="editUserType"></label>
+                <select name="editUserType" id="editUserType">
+                    <option value="System Administrator">System Administrator</option>
+                        <option value="Regular User">Regular User</option>
+                        <option value="Company Administrator">Company Administrator</option>
                 </select>
-                <input type="submit" value="editUser"> 
+                <select name="active" id="active">
+                        <option value="active" name="active">Active</option>
+                        <option value="inactive" name="inactive">Inactive</option>
+                    </select><br>
+                <input type="submit" value="editUser">
+                <input type="hidden" name="action" value="updateUser">
             </form>
-            </c:if>
+        </div>
     </body>
 </html>
